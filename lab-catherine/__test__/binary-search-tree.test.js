55c51006629490d89c77a37f03cd4e12fdc39465
Catherine Looper <loopercat@gmail.com>
2017-12-20 23:11:23 -0800

    Added tests for bst find and insert methods

--- lab-catherine/__test__/binary-search-tree.test.js
+++ lab-catherine/__test__/binary-search-tree.test.js
@@ -12,5 +12,22 @@ describe('testing binary-search-tree.js', () => {
       expect(bst.right.value).toEqual(12);
       expect(bst.left).toBeNull();
     });
+
+    test('testing that insert method will throw an error with invalid input', () => {
+      expect(() => bst.insert(12)).toThrow();
+      expect(() => bst.insert('invalid')).toThrow();
+    });
+  });
+
+  describe('testing that find method functions properly', () => {
+    test('testing that find method will return the requested value of the node', () => {
+      expect(bst.find(10)).toEqual(true);
+      expect(bst.find(12)).toEqual(true);
+    });
+
+    test('testing that find method will return false if value is not in bst', () => {
+      expect(bst.find(1)).toEqual(false);
+      expect(bst.find(3)).toEqual(false);
+    });
   });
 });
